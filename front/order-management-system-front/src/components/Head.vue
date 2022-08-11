6862c3c13c173fd7e619a1542d6d54d76e450dc2
Nciae-Zyh <75648776+Nciae-Zyh@users.noreply.github.com>
2022-08-11 20:54:13 +0800

    页面修改

---
+++ front/order-management-system-front/src/components/Head.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="head">
+    <font-awesome-icon :icon="['fas','map-market']"/>
+    <h3>北华航天</h3>
+    <font-awesome-icon :icon="['fas','caret-down']"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped>
+  @import "./less/head.less";
+
+</style>
--- front/order-management-system-front/src/main.ts
+++ front/order-management-system-front/src/main.ts
@@ -1,7 +1,25 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
+import VueAxios from "vue-axios";
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'api';
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+/* add some free styles */
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+import {faUserSecret} from '@fortawesome/free-solid-svg-icons'
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {faMapMarked} from "@fortawesome/free-solid-svg-icons";
+import {faCaretDown} from "@fortawesome/free-solid-svg-icons";
+import {} from '@fortawesome/free-regular-svg-icons'
+/* add some pro styles */
+/* add each imported icon to the library */
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+library.add(faTwitter, faUserSecret,faSearch,faMapMarked,faCaretDown)
 
 const app = createApp(App)
-app.use(store).use(router).mount('#app')
+app.use(ElementPlus).use(VueAxios, axios).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).mount('#app')
