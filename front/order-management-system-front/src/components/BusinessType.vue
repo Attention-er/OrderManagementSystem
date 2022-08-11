744045f4144d51bffcb3aeb4e5f415c0d9aecc14
Nciae-Zyh <75648776+Nciae-Zyh@users.noreply.github.com>
2022-08-11 20:54:13 +0800

    页面修改

---
+++ front/order-management-system-front/src/components/BusinessType.vue
@@ -0,0 +1,64 @@
+<template>
+  <ul class="business-type">
+    <li v-for="businessType in businessTypes">
+      <img src="@/components/img/dcfl01.png"/>
+      <p>{{ businessType.name }}</p>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const businessTypes = ref()
+businessTypes.value = [
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  },
+  {
+    name: '美食',
+    img: "@/components/img/dcfl01.png"
+  }
+]
+
+</script>
+
+<style lang="less" scoped>
+
+@import "less/businessType.less";
+
+</style>
