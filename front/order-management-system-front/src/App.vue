3cbdafb26e1ba2b99ed187c66f3d837efdf5e95a
Nciae-Zyh <75648776+Nciae-Zyh@users.noreply.github.com>
2022-08-12 21:49:03 +0800

    无js版

--- front/order-management-system-front/src/App.vue
+++ front/order-management-system-front/src/App.vue
@@ -3,16 +3,31 @@
   <Search/>
   <BusinessType/>
   <Banner/>
+  <SuperMember/>
+  <RecommendType/>
+  <Business/>
+  <Foot/>
 </template>
 
-<style lang="less">
-</style>
 <script>
 import Head from "@/components/Head";
 import Search from "@/components/Search";
 import BusinessType from "@/components/BusinessType";
 import Banner from "@/components/Banner";
+import SuperMember from "@/components/SuperMember";
+import Business from "@/components/Business";
+import RecommendType from "@/components/RecommendType";
+import Foot from "@/components/Foot";
+
 export default {
-  components: {Banner, BusinessType, Head,Search}
+  components: {Foot, RecommendType, Business, SuperMember, Banner, BusinessType, Head, Search}
 }
 </script>
+
+
+<style lang="less" scoped>
+*{
+  //margin: 0;
+  padding: 0;
+}
+</style>
